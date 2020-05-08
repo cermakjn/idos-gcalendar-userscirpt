f3a9f14c97024c37494a00389c58e80863d08716
Jan Čermák <info@jan-cermak.cz>
2020-05-08 11:08:10 +0200

    Change file structure (#5)
    * Rename User Script to a proper name
    
    * Create metadata file
    
    Co-authored-by: Jan Cermak <jan.cermak@mallgroup.com>

---
+++ src/idos-add-to-gcalendar.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         IDOS – Uložit spojení do Google Kalendáře
+// @description  Uloží spojení do Google kalendáře
+// @version      1.0.0
+// @icon         https://idos.idnes.cz/favicon.ico
+// @author       Jan Cermak
+// @match        https://idos.idnes.cz/*/spojeni/*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @copyright    2020, Jan Cermak (https://jan-cermak.cz)
+// @licence      MIT
+// @homepageURL  https://github.com/cermakjn/idos-gcalendar-userscirpt
+// @supportURL   https://github.com/cermakjn/idos-gcalendar-userscirpt/issues
+// @contributionURL https://github.com/cermakjn/idos-gcalendar-userscirpt/pulls
+// @updateURL    https://raw.githubusercontent.com/cermakjn/idos-gcalendar-userscirpt/master/src/idos-add-to-gcalendar.meta.js
+// ==/UserScript==
+
+// ==OpenUserJS==
+// @author cermakjn
+// ==/OpenUserJS==
