07705c3a1cbac978f9c3fb44a75cd1a4094b2ef5
Jan Cermak <jan.cermak@mallgroup.com>
2020-05-08 09:52:09 +0200

    Metadata changes - add missing required items for OpenUserJS, CZ language is the default one

--- src/idos-add-to-gcalendar.js
+++ src/idos-add-to-gcalendar.js
@@ -1,14 +1,21 @@
 // ==UserScript==
-// @name         IDOS - Save connection to a Google Calendar
-// @name:cs      IDOS – Uložit spojení do Google Kalendáře
-// @description  Save connection to Google Calendar
-// @description:cs Uloží spojení do Google kalendáře
+// @name         IDOS – Uložit spojení do Google Kalendáře
+// @description  Uloží spojení do Google kalendáře
 // @version      1.0.0
 // @icon         https://idos.idnes.cz/favicon.ico
 // @author       Jan Cermak
 // @match        https://idos.idnes.cz/*/spojeni/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @copyright    2020, Jan Cermak (https://jan-cermak.cz)
+// @licence      MIT
+// @homepageURL  https://github.com/cermakjn/idos-gcalendar-userscirpt
+// @supportURL   https://github.com/cermakjn/idos-gcalendar-userscirpt/issues
+// @contributionURL https://github.com/cermakjn/idos-gcalendar-userscirpt/pulls
 // ==/UserScript==
+
+// ==OpenUserJS==
+// @author cermakjn
+// ==/OpenUserJS==
 
 (function() {
     'use strict';
